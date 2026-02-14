f79896ec8cefade645c50ed768ac5becee09fc7b
Your Name <your-email@example.com>
2026-02-14 21:33:24 +0530

    Feature: University Search & Discovery

--- lib/utils.ts
+++ lib/utils.ts
@@ -4,3 +4,20 @@ import { twMerge } from "tailwind-merge"
 export function cn(...inputs: ClassValue[]) {
     return twMerge(clsx(inputs))
 }
+
+export const COUNTRIES = [
+    "United States",
+    "United Kingdom",
+    "Canada",
+    "Australia",
+    "Germany",
+    "France",
+    "Netherlands",
+    "Sweden",
+    "Switzerland",
+    "Ireland",
+    "New Zealand",
+    "Singapore",
+    "Malaysia",
+    "UAE"
+]
