0476c2379681f852413256e3142f54a3441941b9
jmbuch19 <your-email@example.com>
2026-02-22 21:03:48 +0530

    fix: add trustHost:true to authConfig - fixes UntrustedHost error in Netlify production

--- lib/auth.config.ts
+++ lib/auth.config.ts
@@ -2,6 +2,7 @@
 import type { NextAuthConfig } from "next-auth"
 
 export const authConfig = {
+    trustHost: true,
     pages: {
         signIn: '/login',
         verifyRequest: '/auth/verify-request',
